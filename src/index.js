f1b1499ed972d41922400b766f5e463f8ef6f640
A-lineed <alineedvania2018@gmail.com>
2022-08-19 20:09:30 -0300

    Criação das rotas

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import  express  from "express";
+import { routes } from './routes.js';
 
 const server = express();
+server.use(express.json())
+server.use(routes)
 
 server.listen(8080, () => {
     console.log("Servidor rodando na porta 8080")
 })
+
