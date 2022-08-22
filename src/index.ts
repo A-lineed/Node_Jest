0c69b8a112de338bb6d535d3700f303107fc7658
A-lineed <alineedvania2018@gmail.com>
2022-08-22 19:34:34 -0300

    Configurando o TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import  express  from "express";
-import { routes } from './routes.js';
+
 
 const server = express();
 server.use(express.json())
-server.use(routes)
+
 
 server.listen(8080, () => {
     console.log("Servidor rodando na porta 8080")
